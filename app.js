39807da6ca926a3166f64871b402a0791e3a1491
Joey Ramos <jramos1202@gmail.com>
2020-07-15 23:36:22 -0500

    expand html template to account for more profile data

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-// const fs = require('fs');
+const fs = require('fs');
 
-// const generatePage = require('./src/page-template.js');
+const generatePage = require('./src/page-template');
 
 // const profileDataArgs = process.argv.slice(2);
 
@@ -140,6 +140,12 @@ const promptProject = (portfolioData) => {
 promptUser()
     .then(promptProject)
     .then(portfolioData => {
-        console.log(portfolioData);
-      });
+     const pageHTML = generatePage(portfolioData);
+
+     fs.writeFile('./index.html', pageHTML, err => {
+       if (err) throw new Error(err);
+
+      console.log('Page created! Check out index.html in this directory to see it!');
+     });
+    });
     
